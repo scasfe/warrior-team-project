455fee21e01a849af65ee32426f0c06bf5824a75
yvan.serieye@gmail.com <yvan.serieye@gmail.com@d48e3abc-ceca-14d7-e6bb-0de87b768d68>
2012-01-08 16:34:35 +0000

    Ajout des servlets d'upload oubliées.

---
+++ warriorteam/src/fr/warriorteam/rpc/FileUploadServiceAsync.java
@@ -0,0 +1,13 @@
+package fr.warriorteam.rpc;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import fr.warriorteam.rpc.dto.NewsDTO;
+
+public interface FileUploadServiceAsync {
+
+	void uploadFile(AsyncCallback<List<String>> callback);
+
+}
