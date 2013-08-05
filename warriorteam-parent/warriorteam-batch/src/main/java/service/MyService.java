97c273e01a4b50ecb068109f24a1237059cec81c
yvan.serieye@gmail.com <yvan.serieye@gmail.com@d48e3abc-ceca-14d7-e6bb-0de87b768d68>
2013-08-05 15:51:46 +0000

    Mise en place de l'architecture Spring et Hibernate.

--- warriorteam-parent/warriorteam-batch/src/main/java/service/MyService.java
+++ warriorteam-parent/warriorteam-batch/src/main/java/service/MyService.java
@@ -1,32 +1,10 @@
 package service;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import dao.DaoImpl;
-import dao.IDao;
-
 @Service()
-public class MyService implements IDao{
-	private final Logger logger=Logger.getLogger(this.getClass().getName());
-	private static final long serialVersionUID = 1L;
-	private DaoImpl daoImpl;
-	@Autowired
-	public void setDaoImpl(DaoImpl daoImpl) {
-		this.daoImpl = daoImpl;
-	}
-	public String getClient(final String nom, final String prenom) {		
-		String client= daoImpl.getClient(nom,prenom);
-		logger.info("Client  '"+prenom+"','"+nom+"': "+client);
-		return client;
-	}
-	public List<String> getClients() {		
-		List<String> listeClients= daoImpl.getClients();
-		for(String client:listeClients) {logger.info(client);}
-		return listeClients;
-	}
+public class MyService {
+//	private final Logger logger=Logger.getLogger(this.getClass().getName());
+	
 	
 }
---
+++ warriorteam-parent/warriorteam-service/src/main/java/fr/cr/warriorteam/common/utils/hibernate/WTInterceptor.java
@@ -0,0 +1,12 @@
+package fr.cr.warriorteam.common.utils.hibernate;
+
+import org.hibernate.EmptyInterceptor;
+
+public class WTInterceptor extends EmptyInterceptor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7960133107059680517L;
+
+}
