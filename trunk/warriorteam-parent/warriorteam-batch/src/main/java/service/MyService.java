b47a9451bba7c80507d9ac728a9038d614b9fb11
yvan.serieye@gmail.com <yvan.serieye@gmail.com@d48e3abc-ceca-14d7-e6bb-0de87b768d68>
2014-09-09 18:46:29 +0000

    Mise à jour dépendances Spring. Reprise et maintien du workspace pour eclipse Luna.

--- trunk/warriorteam-parent/warriorteam-batch/src/main/java/service/MyService.java
+++ trunk/warriorteam-parent/warriorteam-batch/src/main/java/service/MyService.java
@@ -1,10 +1,43 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import dao.GenericDao;
+import dao.IDao;
+import fr.cr.warriorteam.common.model.entity.Stock;
 
 @Service()
 public class MyService {
-//	private final Logger logger=Logger.getLogger(this.getClass().getName());
-	
-	
+	// private final Logger logger=Logger.getLogger(this.getClass().getName());
+
+	@Autowired
+	private IDao dao;
+
+	@Autowired
+	private GenericDao stockDao;
+
+	public void getClient(String nom, String prenom) {
+		String client = dao.getClient(nom, prenom);
+		System.out.println(client);
+		Stock stock = new Stock();
+		stock.setStockCode("7668");
+		stock.setStockName("HAIO");
+		stockDao.save(stock);
+
+		/** select **/
+		Stock stock2 = stockDao.findByStockCode("7668");
+		System.out.println(stock2);
+
+		/** update **/
+		stock2.setStockName("HAIO-1");
+		stockDao.update(stock2);
+
+		/** delete **/
+		// stockDao.delete(stock2);
+
+		System.out.println("Done Stock BDD");
+
+	}
+
 }
